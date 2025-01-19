81e13b9153c6e624ea62dda4ca7e5372ada79a1c
Ariel M Diaz <arieldiaz.sistemas@gmail.com>
2025-01-18 22:04:17 -0300

    update migrations

--- crud/database/migrations/2025_01_13_000741_create_payments_table.php
+++ crud/database/migrations/2025_01_13_000741_create_payments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->string('method');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
