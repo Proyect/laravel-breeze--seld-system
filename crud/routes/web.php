20ce9d03304e5e918ac09665788dd28ce1e59eff
Ariel Diaz <arieldiaz.sistemas@gmail.composer>
2025-01-21 12:32:21 -0300

    working with the model and routes - not finished

--- crud/routes/web.php
+++ crud/routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\SalesCon
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +18,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    //Product
+    Route::resource('products', ProductoController::class);
+
+    //Sales
+    Route::resource('sales', SalesController::class);
 });
 
 require __DIR__.'/auth.php';
