6b8b7be6b840a499661cbe3e789ee7cb5423f0ee
Ariel Diaz <arieldiaz.sistemas@gmail.composer>
2025-01-28 21:31:06 -0300

    create the view for product , sales ad pay

--- crud/resources/views/sales/index.blade.php
+++ crud/resources/views/sales/index.blade.php
@@ -1,1 +1,22 @@
-//create table
+<div class="position-absolute  top-50 start-50 text-center d-flex justify-content-center d-none" >
+    <div class="spinner-grow" style="width: 3rem; height: 3rem;" role="status" id="preload">     
+    </div>
+  </div>
+    <div class="container">
+        <!-- Contact Header -->       
+        <!-- Nav -->
+      @yield('container')     
+    </div>    
+
+<!-- Librería jQuery requerida por los plugins de JavaScript -->  
+  	
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   
+    
+    <!-- Data Tables -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script> 
+    <script src="./media/js/table.js"></script>
