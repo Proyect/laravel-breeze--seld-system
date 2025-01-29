f2ae5d42e6c82e641453387da7f333aabc4230e5
Ariel Diaz <arieldiaz.sistemas@gmail.composer>
2025-01-28 21:20:14 -0300

    start to create the views

--- crud/app/Models/SalesDetail.php
+++ crud/app/Models/SalesDetail.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SalesDetail extends Model
 {
-    //
+    public function sales()
+    {
+        return $this->belongsToMany(Sale::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)->withPivot('quantity');
+    }
 }
