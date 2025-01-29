b31788347d93ea5817f83f1ee8c9b192ed6ac025
Ariel Diaz <arieldiaz.sistemas@gmail.composer>
2025-01-28 22:32:50 -0300

    create controller , route and view about users. Update the nav

--- crud/resources/views/layouts/navigation.blade.php
+++ crud/resources/views/layouts/navigation.blade.php
@@ -22,8 +22,8 @@
                     </x-nav-link>    
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{route('products.index')}}" >
-                        Productos
+                    <x-nav-link href="{{route('user.index')}}" >
+                        Users
                     </x-nav-link>    
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
